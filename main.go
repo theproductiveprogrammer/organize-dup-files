a43850cb56853bc3eb33b0812f7cb4d2886a49e7
Charles L <charles.lobo@gmail.com>
2022-12-10 11:55:25 +1100

    feat: get src files

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
+type args struct {
+	src string
+}
+
 func main() {
-		fmt.Println("Hello World")
+	args := args{}
+	loadArgs(&args)
+
+	f_, err := filepath.Glob(args.src)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, fname := range f_ {
+		fmt.Println(fname)
+	}
+}
+
+func loadArgs(args *args) {
+	flag.StringVar(&args.src, "src", "*.*", "the source path (glob patterns allowed)")
+
+	flag.Parse()
 }
